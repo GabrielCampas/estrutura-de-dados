03c1875210b8f3cee16470bd213e5949654d48ef
GABRIEL CAMPANHA <0201392521005>
2026-03-23 12:08:34 -0300

    estrutura de dados com html e js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## 💻 Estrutura de Dados em formulário usando HTML e JavaScript.
 
 <div align="center">
-    <img height="100" widht="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"/>
-    <img height="100" widht="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"/>
+    <img height="60" widht="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"/>
+    <img height="60" widht="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"/>
 </div>
--- js/utilidades.js
+++ js/utilidades.js
@@ -1,60 +1,101 @@
+/* 
+Autor: Alex Batista
+Data: 03/03/2026
+Hora: 08h45min
+
+Arquivo:
+ => utilidades.js (arquivo com funções JavaScript)
+*/
+
 // Função para juntar nome e sobrenome.
 function juntarNome() {
-    let nome = document.getElementById("nome").value;
+    let      nome = document.getElementById("nome").value; 
     let sobrenome = document.getElementById("sobrenome").value;
 
     // 1. Concatenação de strings normal.
     let nomecompleto = nome + ' ' + sobrenome;
 
     // 2. Uso de interpolação strings (template strings).
-    let nomecompleto1 = `<i> ${nome} <br> ${sobrenome} </i>`;
-    let nomecompleto2 = nomecompleto;
-
+    let nomecompleto1 = `<i>${nome} <br>${sobrenome}</i>`;
+    
     // 3. Injetar texto da variável nomecompleto na tag <p> cujo id é "nomecompleto".
     document.getElementById("nomecompleto").innerText = nomecompleto;
 
-    // 4. Injetar o texto da variavel nomecompelto1 na tag <p> cujo id é "nomecompleto1"
+    // 4. Injetar texto da variável nomecompleto1 na tag <p> cujo id é "nomecompleto1".
     document.getElementById("nomecompleto1").innerHTML = nomecompleto1;
 
-    // 5. Injetar texto da variável nomecompleto1 na tag <p> cujo id é "nomecompleto2".
-    document.getElementById("nomecompleto2").textContent = nomecompleto2;
+    // 5. Injetar texto da variável nomecompleto2 na tag <p> cujo id é "nomecompleto2".
+    document.getElementById("nomecompleto2").textContent = nomecompleto;
 }
 
-// mostrando nome num modal
+//  mostrando nome num modal
 function janelaNome() {
-    let nome = prompt(`Entre com seu nome: `);
-    let sobrenome = prompt(`Entre com seu sobrenome: `);
+    let      nome = prompt("Entre com seu nome: ", "Carlos");
+    let sobrenome = prompt("Entre com seu sobrenome: ", "Oliveira");
 
-    // validaçao da entrada de dados 
+    // Validação da entrada de dados de nome e sobrenome.
     if (nome === null || sobrenome === null || nome === "" || sobrenome === "") {
-        document.writeln("<p><strong>Nome e/ou sobrenome vazios.</strong></p>");
-        console.log(`Nome e/ou sobrenome vazios.`);
+            document.writeln("<p><strong>Nome e/ou sobrenome vazios.</strong></p>");
+            console.log("Nome e/ou sobrenome vazios.");
     } else {
-        let nomecompleto1 = `<i> ${nome} <br> ${sobrenome} </i>`;
+        // Uso de interpolação strings (template strings).
+        let nomecompleto1 = `<i>${nome} <br>${sobrenome}</i>`;
 
-        // executar a injeçao de tags + textos
-        document.getElementById(`nomecompleto1`).innerHTML = nomecompleto1;
-        alert(nome + ' ' + sobrenome);
+        // Executar a injeção de tags (elementos HTML) + textos:
+        document.getElementById('nomecompleto1').innerHTML = nomecompleto1;
         console.log(nome + ' ' + sobrenome);
+        alert(nome + ' ' + sobrenome);
     }
+    mostrarLog(nome,sobrenome);
 }
 
-// função pra escrever o mome completo na página web
+// Função para escrever o nome completo na página web (document).
 function escreverNome() {
-    let nome = document.getElementById("nome").value;
+    let      nome = document.getElementById("nome").value; 
     let sobrenome = document.getElementById("sobrenome").value;
+    alerta();
+    let css = "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css'";
+    let resultado = "<strong>" + nome + " " + sobrenome + "</strong><br />";
     
-    // Recarregando o CSS para o ícone aparecer na nova página branca
-    let css = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">';
-
-    let resultado = "<strong>" + nome + " " + sobrenome + "</strong>";
-    document.writeln(resultado);
-    document.writeln("<br><br>");
-    document.writeln("<button id='voltar' onclick='location.reload()'>" + "<i class='fa fa-solid fa fa-arrow-left'></i>Voltar</button>");
+    document.writeln(css);
+    document.writeln(resultado); 
+    document.writeln("<button id='voltar' onclick='location.reload()'>" + 
+                    "<i class='fa-solid fa-arrow-left'></i> Voltar</button>"); 
+    mostrarLog(nome, sobrenome);
 }
 
-// função pra limpar os dados inputados
+// Função para limpar o formulário de dados.
 function limparDados() {
-    // exclui os valores contidos no formulario nomes (nome e sobrenome)
+    // Exclui os valores contidos em nome e sobrenome (inputs).
+    //document.getElementById("nome").value = "";
+    //document.getElementById('sobrenome').value = "";
     
+    // Exclui os valores contidos no formulário nomes (nome e sobrenome).
+    document.getElementById("nomes").reset();
+
+    // Limpar o parágrafo do nomecompleto e nomecompleto1.
+    document.getElementById("nomecompleto").innerText = "";
+    document.getElementById("nomecompleto1").innerHTML = "";
+    document.getElementById("nomecompleto2").textContent = "";
+
+    // Após limpar o formulário e limpar os parágrafos nomecompleto
+    // e nomecompleto1, posicionar o cursor no foco do campo nome.
+    ajustarFoco();
+}
+
+// Função para ajustar o foco no campo nome.
+function ajustarFoco() {
+    document.getElementById("nome").focus();
+}
+
+// Função para mostrar o log de saída de dados.
+function mostrarLog(n,s) {
+    console.log("Nome: " + n);
+    console.log("Sobrenome: " + s);
+    console.log("Nome Completo: " + n + " " + s);
+}
+
+// Função Alerta
+function alerta() {
+    window.alert("Enquanto não fechar esta janela, o nome não vai ser mostrado.");
 }
